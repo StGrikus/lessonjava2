fc50432f4c88a2d653d9595450982c03c52be886
Arthur <saintgrikus@gmail.com>
2022-04-21 21:26:38 +0300

    Serializable p.1.

---
+++ src/WriteObject.java
@@ -0,0 +1,22 @@
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class WriteObject {
+    public static void main(String[] args) {
+        Person person1 = new Person(1, "Arthur");
+        Person person2 = new Person(2, "Anastasia");
+
+        try {
+            FileOutputStream fos = new FileOutputStream("people.bin");
+            ObjectOutputStream oos = new ObjectOutputStream(fos);
+
+            oos.writeObject(person1);
+            oos.writeObject(person2);
+
+            oos.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
